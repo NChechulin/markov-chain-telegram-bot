640dc13b57cda24e2157d23d3ddce70b155afb38
Nikolay Chechulin <nchechulin@protonmail.com>
2020-11-01 17:13:09 +0300

    add basic chain

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+extern crate markov;
+
+use markov::Chain;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let mut chain = Chain::of_order(1);
+    //     for line in chain.iter_for(50) {
+    //         let sentence: String = line.into_iter().map(|word| word + " ").collect();
+    //         println!("{:?}", sentence);
+    //     }
 }
