76a659ffeb70a69ee921649230e734a149dccd3f
Nikolay Chechulin <nchechulin@protonmail.com>
2020-11-01 23:16:37 +0300

    add answering

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use markov::Chain;
 
 use futures::StreamExt;
 use telegram_bot::*;
@@ -6,23 +6,26 @@ use telegram_bot::*;
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let api = Api::new("");
+    let mut chain: Chain<String> = Chain::of_order(1);
 
-    // Fetch new updates via long poll method
     let mut stream = api.stream();
     while let Some(update) = stream.next().await {
-        // If the received update contains a new message...
         let update = update?;
         if let UpdateKind::Message(message) = update.kind {
             if let MessageKind::Text { ref data, .. } = message.kind {
+                // Add message to markov chain
+                chain.feed_str(data);
+
+                let mut response = chain.generate_str_from_token(data);
+                if response.len() == 0 {
+                    response = chain.generate_str();
+                } 
+
                 // Print received text message to stdout.
-                println!("<{}>: {}", &message.from.first_name, data);
+                println!("{} -> {}", data, response);
 
                 // Answer message with "Hi".
-                api.send(message.text_reply(format!(
-                    "Hi, {}! You just wrote '{}'",
-                    &message.from.first_name, data
-                )))
-                .await?;
+                api.send(message.text_reply(response)).await?;
             }
         }
     }
